c6b6fb3a2496c84b867c9baf43c801e9323e30eb
Axonny <sasha.2002056@gmail.com>
2020-09-26 15:48:51 +0500

    second melody

--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -3,10 +3,12 @@
 #include "buzzer.h"
 
 #define PIN_BUTTON_MEL_ONE 3
+#define PIN_BUTTON_MEL_TWO 4
 #define PIN_BUTTON_OFF 5
 #define PIN_BUZZER 6
 
 Button buttonMelodyOne(PIN_BUTTON_MEL_ONE);
+Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
 Button buttonOff(PIN_BUTTON_OFF);
 Buzzer buzzer(PIN_BUZZER);
 
@@ -14,14 +16,13 @@ int notes[] = {NOTE_A4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
 double durations[] = {8, 1, 4, 1};
 int melodyLength = 4;
 
-// maybe somewhere in the future we will have one more button...
-// #define PIN_BUTTON_MEL_TWO 4
-// Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
+int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
+double durations2[] = {4, 1, 4, 1};
+int melodyLength2 = 4;
 
-// and the second melody
-// int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
-// double durations2[] = {4, 1, 4, 1};
-// int melodyLength2 = 4;
+int notes3[] = {NOTE_G4, NOTE_SILENCE, NOTE_E4, NOTE_SILENCE, NOTE_E4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE, NOTE_F4, NOTE_SILENCE, NOTE_E4, NOTE_SILENCE, NOTE_D4, NOTE_SILENCE, NOTE_C4};
+double durations3[] {8, 1, 4, 1, 4, 1, 4, 1, 4, 1, 4, 1, 4, 1, 4};
+int melodyLength3 = 15;
 
 void setup()
 {
@@ -35,6 +36,11 @@ void loop()
     if (buttonOff.wasPressed())
     {
         buzzer.turnSoundOff();
+    }
+
+    if (buttonMelodyTwo.wasPressed()){
+      buzzer.setMelody(notes3, durations3, melodyLength3);
+        buzzer.turnSoundOn();
     }
 
     if (buttonMelodyOne.wasPressed())
